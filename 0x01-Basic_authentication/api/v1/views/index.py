80a10c2418d708ce283429da63e3d155f5e00f67
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-06-05 12:38:27 +0100

    Implementation of Unauthorized route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized/", strict_slashes=False)
+def unauthorized() -> str:
+      """Protected Route"""
+      abort(401, description="Unauthorized resource")
