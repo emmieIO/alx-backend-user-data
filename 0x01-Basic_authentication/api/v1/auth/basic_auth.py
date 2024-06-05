82c19ce0ece807efae4ac25c2d8a49f58b69904e
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-06-05 22:06:18 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
-"""api/v1/auth/basic_auth.py"""
+"""
+Definition of class BasicAuth
+"""
+import base64
 from .auth import Auth
+from typing import TypeVar
+
+from models.user import User
 
 
 class BasicAuth(Auth):
+    """ Implement Basic Authorization protocol methods
     """
-    Basic Auth Config Class
-    """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """base64 authorization_header extractor"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        token = authorization_header.split(" ")[-1]
+        return token
